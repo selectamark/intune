e34bda8b2aa369d45ccbb60451bd35a9b55254fd
Will Nichols <wgen@msn.com>
2022-12-01 17:34:45 +0000

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -5,7 +5,7 @@ $templateFilePath = "$DIR\startup.ps1"
 
 $stt = New-ScheduledTaskTrigger -AtLogOn
 $sta = New-ScheduledTaskAction -Execute "conhost.exe --headless powershell.exe -ExecutionPolicy Bypass $templateFilePath"
-$stp = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators"
+$stp = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
 
 Register-ScheduledTask SM_Task01 -Trigger $stp -Action $sta -Principal $stp -Force
 
