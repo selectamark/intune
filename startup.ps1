b9c3eb84853bf8a5b93ca574ba665790393c3f1a
Will Nichols <wgen@msn.com>
2022-12-01 17:30:49 +0000

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,1 +1,12 @@
+$DIR = "C:\ProgramData\Selectamark"
+[system.io.directory]::CreateDirectory("$DIR")
+
+$templateFilePath = "$DIR\startup.ps1"
+
+$stt = New-ScheduledTaskTrigger -AtLogOn
+$sta = New-ScheduledTaskAction -Execute "conhost.exe --headless powershell.exe -ExecutionPolicy Bypass $templateFilePath"
+$stp = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators"
+
+Register-ScheduledTask SM_Task01 -Trigger $stp -Action $sta -Principal $stp -Force
+
 winget uninstall "Xbox Game Bar"
