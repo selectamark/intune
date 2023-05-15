ca1f198d11e8334b34c7a031b90363f0bce35217
Will Nichols <wgen@msn.com>
2023-05-15 16:32:41 +0100

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -17,6 +17,17 @@ Register-ScheduledTask SM_LoginTask -Trigger $stt -Action $sta -Principal $stp -
 
 for(;;) {
   if(Get-Command winget -ErrorAction SilentlyContinue) {
+  
+    $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+            Select-Object -ExpandProperty "assets" |
+            Where-Object "browser_download_url" -Match '.msixbundle' |
+            Select-Object -ExpandProperty "browser_download_url"
+
+    Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+    Add-AppxPackage -Path "Setup.msix"
+    Remove-Item "Setup.msix"
+    
     break
   } else {
     sleep 15
